7894f5807ae26ffc800a1446ab39f06ca88f064b
vinopermana77 <vinopermana13@gmail.com>
2024-01-23 13:32:06 +0700

    add mass assignment

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employee extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'gender',
+        'age',
+        'phone',
+        'photo',
+        'team_id',
+        'role_id',
+        'is_verified',
+        'verified_at',
+    ];
 }
